038b4af448444f56172c4c653a2bc8042f96cb88
Benjamin BRASSART <bbrassar@student.42.fr>
2021-09-30 06:19:54 +0200

    Fixed norme issue

--- src/client/on_success.c
+++ src/client/on_success.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 07:24:16 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/09/29 09:31:58 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/09/30 06:19:14 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	on_success(int sig)
 	(void)sig;
 	if (++i >= 8)
 	{
-		if(*_mt()->message == 0)
+		if (*_mt()->message == 0)
 		{
 			ft_printf("Received confirmation from server.\n");
 			exit(0);
