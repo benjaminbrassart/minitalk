64625cf6b84a8b7de1aec271b8ba015b279ff3c8
Benjamin BRASSART <bbrassar@student.42.fr>
2021-09-30 06:18:29 +0200

    Added error message if send_byte failed

--- src/client/main.c
+++ src/client/main.c
@@ -6,25 +6,33 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 02:51:05 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/09/29 09:23:22 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/09/30 06:13:18 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stdlib.h"
+#include "ft_stdio.h"
 #include "minitalk.h"
 #include "minitalk_client.h"
+#include <stdlib.h>
 
 int	main(int argc, char *argv[])
 {
 	t_mt *const	mt = _mt();
 	int			pid;
+	int			i;
 
 	check_args(argc, argv);
 	pid = ft_atoi(argv[1]);
 	mt->message = argv[2];
 	while (*mt->message)
 	{
-		send_byte(pid, *mt->message);
+		i = send_byte(pid, *mt->message);
+		if (i != 8)
+		{
+			ft_dprintf(2, "Failed to send byte to server with pid %d.\n", pid);
+			exit(1);
+		}
 		++(mt->message);
 	}
 	send_byte(pid, 0);
