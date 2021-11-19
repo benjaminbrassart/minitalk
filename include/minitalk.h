907ef09f133ca153bf64058035339d7dd8eaf473
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-19 05:47:50 +0100

    Added check if macro SLEEP_TIME is already defined

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,13 +6,15 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 07:30:35 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/11/18 00:16:14 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/19 05:29:37 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# define SLEEP_TIME 1000
+# ifndef SLEEP_TIME
+#  define SLEEP_TIME 1000
+# endif
 
 #endif
