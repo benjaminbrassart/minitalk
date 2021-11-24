612cb80dbf54ae9d765323053332a63ae6475b7f
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-24 18:51:22 +0100

    Fixed buffer flush

--- src/server/server_buffer_flush.c
+++ src/server/server_buffer_flush.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 23:35:50 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/11/17 23:44:53 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/24 17:42:33 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,10 @@ void	server_buffer_flush(void)
 		return ;
 	if (server->message)
 		size += ft_strlen(server->message);
-	new_message = ft_calloc(size + 1, sizeof (*new_message));
-	if (new_message)
+	if (ft_pcalloc(size + 1, sizeof (*new_message), (void **)&new_message))
 	{
-		ft_strncat(new_message, server->buffer, server->buffer_size);
 		ft_strncat(new_message, server->message, size - server->buffer_size);
+		ft_strncat(new_message, server->buffer, server->buffer_size);
 		free(server->message);
 		server->message = new_message;
 		server_buffer_clear();
