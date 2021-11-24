262128e9797483573bebd3ec6aed1b0559636f3f
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-24 18:51:58 +0100

    Changed SLEEP_TIME macro depending on the platform

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 07:30:35 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/11/19 05:29:37 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/24 17:48:44 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 # define MINITALK_H
 
 # ifndef SLEEP_TIME
-#  define SLEEP_TIME 1000
+#  ifdef __APPLE__
+#   define SLEEP_TIME 100
+#  else
+#   define SLEEP_TIME 400
+#  endif
 # endif
 
 #endif
