a7e698cf41a0acbcaca5f7dd36f169c5c7f58717
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-25 23:10:03 +0100

    Reduced SLEEP_TIME macro for MacOS. See later to remove sleeps

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 07:30:35 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/11/24 17:48:44 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/25 23:08:32 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # ifndef SLEEP_TIME
 #  ifdef __APPLE__
-#   define SLEEP_TIME 100
+#   define SLEEP_TIME 30
 #  else
 #   define SLEEP_TIME 400
 #  endif
